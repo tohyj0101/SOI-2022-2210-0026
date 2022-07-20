823115998c30cc23f8c1e12ab360fbd64ffd63d0
tohyj0101 <83193430+tyj0101@users.noreply.github.com>
2022-07-20 20:09:27 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -107,25 +107,3 @@ cuckoo web -H 0.0.0.0 -p 8000 &> ~/Desktop/cuckoo_web.log &
 echo -e "\033[42;30m-----------------------------------------------\033[0m"
 echo -e "\033[42;30m Done! Cuckoo web service running on port 8000 \033[0m"
 echo -e "\033[42;30m-----------------------------------------------\033[0m"
-
-#!/bin/bash
-#
-# Start analysis
-
-function startVM() {
-	vboxmanage startvm "cuckoo1" --type headless
-}
-
-function startCuckoo() {
-	cuckoo
-}
-
-echo -e "\033[41;30m------------------------------\033[0m"
-echo -e "\033[41;30m Start VM (cuckoo1) \033[0m"
-echo -e "\033[41;30m------------------------------\033[0m"
-startVM
-
-echo -e "\033[41;30m------------------------------\033[0m"
-echo -e "\033[41;30m Start Cuckoo \033[0m"
-echo -e "\033[41;30m------------------------------\033[0m"
-startCuckoo
