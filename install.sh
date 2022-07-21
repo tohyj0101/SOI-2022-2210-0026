03426df9335ee062a654fb027a44748716c2ca11
tohyj0101 <83193430+tyj0101@users.noreply.github.com>
2022-07-21 20:30:04 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#
+# Automatically deploy a Cuckoo sandbox
+
+function downloadAgent() {
+	# Agent.ova is a Windows 7 (x86) virtual machine file and is used as a Cuckoo agent
+	url="https://drive.google.com/u/0/uc?id=1uGxNwvSuSIhokeuX9N61D8VtyFDoK0-2&export=download"
+	# Download Agent.ova from google drive (Manually download if not working)
+	wget $url -O ~/Downloads/Agent.ova
+}
+
+function installDependencies() {
+	# Install system updates and update system dependencies
+	sudo apt-get update -y && sudo apt-get upgrade -y
+	# https://askubuntu.com/questions/339790/how-can-i-prevent-apt-get-aptitude-from-showing-dialogs-during-installation/340846
+	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+	# Install basic system dependencies by Cuckoo required (if an iptables-persistent configuration GUI window pops up, keep the default selection and proceed)
+	sudo apt-get install -y virtualbox vim curl net-tools htop python python-pip python-dev libffi-dev libssl-dev python-virtualenv python-setuptools python-magic python-libvirt ssdeep libjpeg-dev zlib1g-dev swig mongodb postgresql libpq-dev build-essential git libpcre3 libpcre3-dev libpcre++-dev libfuzzy-dev automake make libtool gcc tcpdump dh-autoreconf flex bison libjansson-dev libmagic-dev libyaml-dev libpython2.7-dev tcpdump apparmor-utils iptables-persistent
+	# Update PIP
+	sudo pip install --upgrade pip
+	# Install Python dependencies
+	sudo pip install -U gdown==3.10.0 sqlalchemy==1.3.3 pefile==2019.4.18 pyrsistent==0.17.0 dpkt==1.8.7 jinja2==2.9.6 pymongo==3.0.3 bottle yara-python==3.6.3 requests==2.13.0 python-dateutil==2.4.2 chardet==2.3.0 setuptools psycopg2 pycrypto pydeep distorm3 cuckoo==2.0.7 weasyprint==0.36 m2crypto openpyxl ujson pycrypto pytz pyOpenSSL
+	# Uninstall Werkzeug and reinstall Werkzeug 0.16.1 (Werkzeug has a version update which leads to incompatibility with Cuckoo 2.0.7)
+	sudo pip uninstall --yes werkzeug && sudo pip install werkzeug==0.16.1
+	# Install the pySSDeep, yara, and volatility plugins
+	git clone https://github.com/bunzen/pySSDeep.git ~/Downloads/pySSDeep && cd ~/Downloads/pySSDeep && sudo python setup.py build && sudo python setup.py install && cd ~
+	wget https://github.com/VirusTotal/yara/archive/v3.7.1.tar.gz -O ~/Downloads/v3.7.1.tar.gz && tar -xzvf ~/Downloads/v3.7.1.tar.gz -C ~/Downloads && cd ~/Downloads/yara-3.7.1 && sudo ./bootstrap.sh && sudo ./configure --with-crypto --enable-cuckoo --enable-magic && sudo make && sudo make install && cd ~
+	git clone https://github.com/volatilityfoundation/volatility.git ~/Downloads/volatility && cd ~/Downloads/volatility && sudo python ./setup.py build && sudo python ./setup.py install && cd ~
+}
+
+function configureVirtualbox() {
+	# Use VboxManage to create a hostonly ethernet adapter vboxnet0
+	vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 --netmask 255.255.255.0
+	# Modify the default storage directory and permission for virtual machine files
+	sudo mkdir /data && sudo mkdir /data/VirtualBoxVms && sudo chmod 777 /data/VirtualBoxVms
+	vboxmanage setproperty machinefolder /data/VirtualBoxVms
+	# Import Agent.ova, boot up to take a snapshot, add it to Cuckoo and revert the snapshot
+	vboxmanage import $1 && vboxmanage modifyvm "Agent" --name "cuckoo1" && vboxmanage startvm "cuckoo1" --type headless
+	sleep 3m
+	vboxmanage snapshot "cuckoo1" take "snap1" && vboxmanage controlvm "cuckoo1" poweroff && vboxmanage snapshot "cuckoo1" restorecurrent
+}
+
+function configureNetwork() {
+	# Configure tcpdump
+	sudo aa-disable /usr/sbin/tcpdump && sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+	# Configure iptables to enable IP forwarding
+	sudo iptables -A FORWARD -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT && sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT && sudo iptables -A POSTROUTING -t nat -j MASQUERADE
+	sudo sed -i "s/#net.ipv4.ip_forward=/net.ipv4.ip_forward=/g" /etc/sysctl.conf
+	# Configure persistency
+	sudo sysctl -p /etc/sysctl.conf && sudo netfilter-persistent save
+	# Configure system DNS
+	sudo sed -i "s/127.0.0.53/8.8.8.8/g" /etc/resolv.conf
+
+}
+
+function configCuckoo() {
+	# Start the MongoDB service and Cuckoo, then pull community signatures
+	sudo service mongodb start && cuckoo && cuckoo community
+	# Add Agent to Cuckoo
+	cuckoo machine --delete cuckoo1 && cuckoo machine --add cuckoo1 192.168.56.5 --platform windows --snapshot snap1
+	# Start MongoDB and VirusTotal
+	sed "45d" ~/.cuckoo/conf/reporting.conf > ~/.cuckoo/conf/tmp.conf && sed -i "/mongodb]/a\enabled = yes" ~/.cuckoo/conf/tmp.conf && rm -rf ~/.cuckoo/conf/reporting.conf && mv ~/.cuckoo/conf/tmp.conf ~/.cuckoo/conf/reporting.conf
+	sed "148d" ~/.cuckoo/conf/processing.conf > ~/.cuckoo/conf/tmp.conf && sed -i "/virustotal]/a\enabled = yes" ~/.cuckoo/conf/tmp.conf && rm -rf ~/.cuckoo/conf/processing.conf && mv ~/.cuckoo/conf/tmp.conf ~/.cuckoo/conf/processing.conf
+}
+
+echo -e "\033[41;30m-----------------------------------------\033[0m"
+echo -e "\033[41;30m Step 1: Install and update dependencies \033[0m"
+echo -e "\033[41;30m-----------------------------------------\033[0m"
+installDependencies
+
+echo -e "\033[41;30m--------------------------------------------------\033[0m"
+echo -e "\033[41;30m Step 2: Download Agent.ova virtual machine file  \033[0m"
+echo -e "\033[41;30m--------------------------------------------------\033[0m"
+downloadAgent
+
+echo -e "\033[41;30m------------------------------\033[0m"
+echo -e "\033[41;30m Step 3: Configure VirtualBox \033[0m"
+echo -e "\033[41;30m------------------------------\033[0m"
+configureVirtualbox ~/Downloads/Agent.ova
+
+echo -e "\033[41;30m---------------------------\033[0m"
+echo -e "\033[41;30m Step 4: Configure Network \033[0m"
+echo -e "\033[41;30m---------------------------\033[0m"
+configureNetwork
+
+echo -e "\033[41;30m--------------------------\033[0m"
+echo -e "\033[41;30m Step 5: Configure Cuckoo \033[0m"
+echo -e "\033[41;30m--------------------------\033[0m"
+configCuckoo
+
+echo -e "\033[41;30m-----------------------------\033[0m"
+echo -e "\033[41;30m Step 6: Run Cuckoo services \033[0m"
+echo -e "\033[41;30m-----------------------------\033[0m"
+cuckoo &> ~/Desktop/cuckoo.log &
+cuckoo web -H 0.0.0.0 -p 8000 &> ~/Desktop/cuckoo_web.log &
+
+echo -e "\033[42;30m-----------------------------------------------\033[0m"
+echo -e "\033[42;30m Done! Cuckoo web service running on port 8000 \033[0m"
+echo -e "\033[42;30m-----------------------------------------------\033[0m"
